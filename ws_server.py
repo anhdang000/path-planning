ebba4a29f8a50ee45f2b6e222cdb92543ddb5c04
anhdang000 <danglinh.anh000@gmail.com>
2022-12-07 22:02:58 +0700

    ws now serves both send and receive

--- ws_server.py
+++ ws_server.py
@@ -1,36 +1,24 @@
 import asyncio
 import websockets
-import time
-
-
-def reading_log_files(filename):
-    with open(filename, "r") as f:
-        data = f.read().splitlines()
-    return data
-
-
-def log_generator(filename, period=0.1):
-    data = reading_log_files(filename)
-    while True:
-        time.sleep(period)
-        new_data = reading_log_files(filename)
-        yield new_data[len(data):]
-        data = new_data
+from utils import log_generator
 
 
 async def handler(websocket):
-    x = log_generator("transfer_data.txt")
+    x = log_generator("transfer_data/send.txt")
     for lines in x:
-        # msg = await websocket.recv()
-        # print(f"Receive: {msg}")
         if len(lines) > 0:
-            print(f"Send: {lines[-1]}")
+            print(f'Send: {lines[-1]}')
             await websocket.send(lines[-1] + '\n')
+        try:
+            msg = await asyncio.wait_for(websocket.recv(), timeout=0.00001)
+            print(f'Receive: {msg}')
+            with open('transfer_data/receive.txt', 'w') as f:
+                f.write(msg + '\n')
+        except:
+            pass
 
 
-async def main():
-    async with websockets.serve(handler, "0.0.0.0", 8000):
-        await asyncio.Future()  # run forever
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(handler, '0.0.0.0', 8000))
 
-
-asyncio.run(main())
+asyncio.get_event_loop().run_forever()
